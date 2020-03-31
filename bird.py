08344e77ccd13389830f3cfec4f118f1c7978077
Jiawei Wang <wangjiawei_med@hotmail.com>
2020-03-31 00:01:56 -0400

    Create Bird class

---
+++ bird.py
@@ -0,0 +1,68 @@
+import pygame
+import neat
+import time
+import os
+import random
+
+# Size of the screen
+WIN_WIDTH = 600
+WIN_HEIGHT = 800
+
+
+# Load image files
+BIRD_IMGS = [  # double the size; load the image; combine the path
+    pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+
+
+class Bird:
+    IMGS = BIRD_IMGS
+    MAX_ROTATION = 25  # Bird tilt angle
+    ROT_VEL = 20  # Rotation per frame
+    ANIMATION_TIME = 5
+
+    # Starting position
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tick_count = 0
+        self.vel = 0
+        self.height = self.y
+        self.img_count = 0
+        self.img = self.IMGS[0]
+
+    def jump(self):
+        self.vel = -10.5
+        self.tick_count = 0
+        self.height = self.y
+
+    def move(self):
+        self.tick_count += 1
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
